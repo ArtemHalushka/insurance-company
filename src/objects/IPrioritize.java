a7f90e39b5a5b8d1b0b4061e78e208b98712bb18
ArtemHalushka <148883360+ArtemHalushka@users.noreply.github.com>
2023-11-08 23:48:28 -0800

    Update src/objects/IPrioritize.java
    Co-authored-by: ankochergin <103670717+ankochergin@users.noreply.github.com>

--- src/objects/IPrioritize.java
+++ src/objects/IPrioritize.java
@@ -3,17 +3,11 @@ package objects;
 public interface IPrioritize {
 
     String HIGH_LEVEL = "High level";
-
     String MEDIUM_LEVEL = "Medium level";
-
     String LOW_LEVEL = "Low level";
-
     int VEHICLE_LEVEL_YEAR = 2010;
-
     int VEHICLE_LEVEL_PRICE = 9999;
-
     int HOME_LEVEL_PRICE = 59999;
-
     int HOME_LEVEL_M2 = 30;
 
     String insuranceObjectLevel();
