b2e674bffd22439a9cc57239091a0f126a649d29
Artem Halushka <ahalushka.laba@solvd.com>
2023-11-10 07:38:25 -0800

    format

--- src/company/InsuranceCompany.java
+++ src/company/InsuranceCompany.java
@@ -22,30 +22,26 @@ public class InsuranceCompany implements ICompanyManage, IIssue, IRequest {
     }
 
     @Override
-    public Insurance issueInsurance(InsuranceRequest request, Employee employee, String insuranceName, Date issueDate,
-                                    Date endDate) throws InvalidPriceException, InvalidM2Exception, InvalidInsureObjectException {
+    public Insurance issueInsurance(InsuranceRequest request, Employee employee, String insuranceName, Date issueDate, Date endDate) throws InvalidPriceException, InvalidM2Exception, InvalidInsureObjectException {
         if (request.getCustomer().getInsureObject() instanceof Vehicle) {
             if (((Vehicle) request.getCustomer().getInsureObject()).getPrice() < IInsuranceObjectParameter.INVALID_VEHICLE_PRICE) {
                 throw new InvalidPriceException("Wrong price. Please use greater than " + IInsuranceObjectParameter.INVALID_VEHICLE_PRICE);
             }
-            VehicleInsurance insurance = new VehicleInsurance(insuranceName, employee, request.getCustomer(),
-                    issueDate, endDate, (Vehicle) request.getCustomer().getInsureObject());
+            VehicleInsurance insurance = new VehicleInsurance(insuranceName, employee, request.getCustomer(), issueDate, endDate, (Vehicle) request.getCustomer().getInsureObject());
             insuranceCount++;
             return insurance;
         } else if (request.getCustomer().getInsureObject() instanceof Home) {
-                if (((Home) request.getCustomer().getInsureObject()).getPrice() < IInsuranceObjectParameter.INVALID_HOME_PRICE) {
-                    throw new InvalidPriceException("Wrong price. Please use greater than " + IInsuranceObjectParameter.INVALID_HOME_PRICE);
-                }
-                if (((Home) request.getCustomer().getInsureObject()).getM2() < IInsuranceObjectParameter.INVALID_HOME_M2) {
-                    throw new InvalidM2Exception("Wrong M2. Please use greater than " + IInsuranceObjectParameter.INVALID_HOME_M2);
-                }
-                HomeInsurance insurance = new HomeInsurance(insuranceName, employee, request.getCustomer(),
-                        issueDate, endDate, (Home) request.getCustomer().getInsureObject());
-                insuranceCount++;
-                return insurance;
+            if (((Home) request.getCustomer().getInsureObject()).getPrice() < IInsuranceObjectParameter.INVALID_HOME_PRICE) {
+                throw new InvalidPriceException("Wrong price. Please use greater than " + IInsuranceObjectParameter.INVALID_HOME_PRICE);
+            }
+            if (((Home) request.getCustomer().getInsureObject()).getM2() < IInsuranceObjectParameter.INVALID_HOME_M2) {
+                throw new InvalidM2Exception("Wrong M2. Please use greater than " + IInsuranceObjectParameter.INVALID_HOME_M2);
+            }
+            HomeInsurance insurance = new HomeInsurance(insuranceName, employee, request.getCustomer(), issueDate, endDate, (Home) request.getCustomer().getInsureObject());
+            insuranceCount++;
+            return insurance;
         } else if (request.getCustomer().getInsureObject() instanceof Health) {
-            HealthInsurance insurance = new HealthInsurance(insuranceName, employee, request.getCustomer(),
-                    issueDate, endDate);
+            HealthInsurance insurance = new HealthInsurance(insuranceName, employee, request.getCustomer(), issueDate, endDate);
             insuranceCount++;
             return insurance;
         }
