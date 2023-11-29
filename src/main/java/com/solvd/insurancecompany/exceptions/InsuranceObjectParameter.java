18c61e421db6cdef34016260c6b8ea62dfc1877d
ArtemHalushka <148883360+ArtemHalushka@users.noreply.github.com>
2023-11-29 00:29:58 -0800

    Update src/main/java/com/solvd/insurancecompany/exceptions/InsuranceObjectParameter.java
    Co-authored-by: ankochergin <103670717+ankochergin@users.noreply.github.com>

--- src/main/java/com/solvd/insurancecompany/exceptions/InsuranceObjectParameter.java
+++ src/main/java/com/solvd/insurancecompany/exceptions/InsuranceObjectParameter.java
@@ -2,10 +2,10 @@ package com.solvd.insurancecompany.exceptions;
 
 public enum InsuranceObjectParameter {
 
-    INVALID_VEHICLE_PRICE(500),
-    INVALID_HOME_PRICE(2000),
-    INVALID_HOME_M2(5),
-    INVALID_STRING_LENGTH(50);
+    MIN_VEHICLE_PRICE(500),
+    MIN_HOME_PRICE(2000),
+    MIN_HOME_M2(5),
+    MAX_STRING_LENGTH(50);
 
     private final double value;
 
